74daf2c8b9b5753c1c4623bfe074dea03652f45b
Slava Rossiytsev <slavarossiytsev@gmail.com>
2019-10-11 23:52:14 +0300

    Add order system include controller, views, db table, tests and related

--- SimpleSportsStore/Models/ApplicationDbContext.cs
+++ SimpleSportsStore/Models/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace SimpleSportsStore.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ SimpleSportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace SimpleSportsStore.Models
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
