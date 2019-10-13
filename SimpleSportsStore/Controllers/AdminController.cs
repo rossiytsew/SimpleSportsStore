b625f6bc368e1947e2707f92d364d810d5ff5c04
Slava Rossiytsev <slavarossiytsev@gmail.com>
2019-10-14 02:58:11 +0300

    begin crud and some testing for this, add server side form checking

---
+++ SimpleSportsStore/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleSportsStore.Models;
+using System.Linq;
+
+namespace SimpleSportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+
+
+        public ViewResult Edit(int productId) =>
+            View(repository.Products
+                .FirstOrDefault(p => p.ProductID == productId));
+
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
--- SimpleSportsStore/Models/IProductRepository.cs
+++ SimpleSportsStore/Models/IProductRepository.cs
@@ -5,5 +5,6 @@ namespace SimpleSportsStore.Models
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
     }
 }
